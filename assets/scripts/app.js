93f7e32a26f34f2351fc72da2b6cb131fbc72760
OlivierZiolkowski <olivier.ziolkowski@gmail.com>
2021-07-09 10:14:14 +0200

    Result board: improve setTimeOut method integrating gameRules & init result message after reboot the game

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -15,18 +15,17 @@ const playerPick = document.querySelector("#player-pick");
 const computerPick = document.querySelector("#computer-pick");
 
 const app = {
-    // Game initialization
+    //* Game initialization
     init: function () {
         // Upload saving player score function
         score.init();
         // Launch game function
         app.playerPick();
-
         // Defines event on replay button
         replayButton.addEventListener("click", app.relaunchGame);
     },
+    //* Permit to launch a new game when player click on a game token
     playerPick: function () {
-        // Permit to launch a new game when player click on a game token
         for (let i = 0; i < gameboardItemsList.length; i++) {
             // Add each choice on choiceList array
             choicesList.push(gameboardItemsList[i].classList[1]);
@@ -34,6 +33,7 @@ const app = {
             gameboardItemsList[i].addEventListener("click", app.launchGame);
         }
     },
+    //* Launch a new game
     launchGame: function (clickedItem) {
         // Retrieves class from player's choice pick
         playerChoice = clickedItem.path[1].classList[1] ? clickedItem.path[1].classList[1] : clickedItem.path[0].classList[1] ;
@@ -52,15 +52,17 @@ const app = {
         // Call computer choice
         app.computerChoice();
     },
+    //* Computer's choice
     computerChoice: function () {
         // Defines how computer make a choice
         let computerSaid = choicesList[Math.floor(Math.random() * choicesList.length)];
         // Defines a timeout to async computer's choice
-        setTimeout(function(){ computerPick.classList.add(computerSaid)}, 2000);
-
-        // Apply rules after computer's choice
-        rules.gameRules();
+        setTimeout(function(){
+            computerPick.classList.add(computerSaid),
+            rules.gameRules();
+        }, 2000);
     },
+    //* Reboot game
     relaunchGame: function () {
         // Gameboard init
         gameboard.style.left = 0;
@@ -71,6 +73,9 @@ const app = {
         resultBoard.style.right = "-200%";
         resultBoard.style.opacity = 0;
         resultBoard.style.transition = "all 0.6s ease-in-out";
+
+        // Result message init
+        infosBlock.style.display = "none";
 
         // Remove class on result board tokens
         playerPick.classList.remove("paper", "scissors", "rock", "win");
